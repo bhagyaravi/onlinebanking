3eab4db97c94b240b9aaab8b632caf5bc4540ade
bhagyaravi <bhagyaravi98@gmail.com>
2020-07-09 12:21:16 +0530

    Update CustomerResource.java

--- src/main/java/com/test/quarkus/rest/CustomerResource.java
+++ src/main/java/com/test/quarkus/rest/CustomerResource.java
@@ -24,6 +24,7 @@ public class CustomerResource {
 	        return "hello customer";
 	    }
 	    @GET
+	    @Path("/list")
 	    @Produces(MediaType.APPLICATION_JSON)
 	    public Response getAllCustomers() {
 	        return Response.ok(Customer.findAll()).build();
